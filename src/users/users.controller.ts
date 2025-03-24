852e0f05be3526c12dd3ace87d732b52ed1b3e43
Hanna <hannah10211421@gmail.com>
2025-03-24 13:04:54 +0530

    controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,40 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, Query } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+
+    @Get()   // GET /users
+    findAll(){
+        return []
+    }
+
+    // @Get()   // GET /users?role=value
+    // findAll(@Query('role') role?: 'Admin' | 'user'){
+    //     return []
+    // }
+
+    @Get(':id')   // GET /users/:id
+    findOne(@Param('id') id: string){
+        return {id}  //return id value
+    }
+
+    // @Get('interns')   // GET /users/:id
+    // findAllInterns(){
+    //     return []  // if write after the above crud operation, it returns "id: interns" instead of []. So use it before above operation
+    // }
+
+    @Post()
+    create(@Body() user: {name: string, email: string, role: 'Admin' | 'user'}){    // read body of the request
+        return user
+    }
+
+    @Put(':id')   // GET /users/:id
+    update(@Param('id') id: string, @Body() userUpdate: {}){
+        return {id, ...userUpdate}  //return id value
+    }
+
+    @Delete(':id')   // GET /users/:id
+    delete(@Param('id') id: string){
+        return {id}  //return id value
+    }
+}
